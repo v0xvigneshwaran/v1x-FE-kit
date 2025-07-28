affdaee62c551b61c004be2c6ee890ae8e3fe2ac
v0xvigneshwaran <vigneshwaran@vezham.com>
2025-07-29 03:57:50 +0530

    tw4 - poc Initial commit

--- apps/ts4/src/app/app.tsx
+++ apps/ts4/src/app/app.tsx
@@ -5,7 +5,7 @@ import { Route, Routes, Link } from 'react-router-dom';
 export function App() {
   return (
     <div>
-      <NxWelcome title="@v1x-fe-kit/ts4" />
+      <NxWelcome title="@v1xfe-kit/ts4" />
 
       {/* START: routes */}
       {/* These routes and navigation have been generated for you */}
